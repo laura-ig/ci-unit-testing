50cd162f1c8bf39ea6b2a14a71122d9d91b273a1
Laura <laura.vidal@internaliagroup.com>
2023-02-16 21:18:02 +0100

    change from master

--- src/api.ts
+++ src/api.ts
@@ -3,4 +3,4 @@ import { Credential, User } from './model';
 // TODO: Implement real login method on backend server
 export const login = (credential: Credential): Promise<User> => {
   return Promise.reject('Pending to implement');
-};
+};//
